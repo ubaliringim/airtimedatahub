fa681e24975812290d0fdf1388c1464eac7dba9d
Engr. Auwal Muhammad Ubali <ubaliiringim@gmail.com>
2025-05-24 10:58:23 +0100

    Fix Supabase upsert and add new user fields

--- api/register.mjs
+++ api/register.mjs
@@ -27,7 +27,8 @@ export default async function handler(req, res) {
           first_name: telegramUser.first_name,
           last_name: telegramUser.last_name,
           wallet_balance: 0,
-          email: telegramUser.email
+          email: telegramUser.email,
+          phone_number: telegramUser.phone_number
         }
       ], { onConflict: 'telegram_id' });
 
